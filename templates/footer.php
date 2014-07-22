64aa4d3d99da31f57012e99fab2d31d4edc462b2
Jesús Torres <jmtorres@ull.es>
2014-07-22 16:00:22 +0100

    reorganización de la plantilla del pié

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,9 @@
 <?php global $ss_framework; ?>
 <footer id="footer" class="content-info" role="contentinfo">
-	<?php
-		echo $ss_framework->open_container( 'div' );
+	<?php echo $ss_framework->open_container( 'div' );
 		echo $ss_framework->open_row( 'div' );
-		echo $ss_framework->open_col( 'div', array( 'mobile' => 12 ) );
-	?>
+		echo $ss_framework->open_col( 'div', array( 'mobile' => 12 ) );?>
+
 		<ul class="address pull-left">
 			<li><strong>Universidad de La Laguna</strong><br> Pabellón de Gobierno, C/ Molinos de Agua s/n. | San Cristóbal de La Laguna, Santa Cruz de Tenerife - España (38200) | Teléfono: (+34) 922 31 90 00/01</li>
 		</ul>
@@ -16,9 +15,8 @@
 			<li><a title="LinkedIn" target="_blank" href="http://www.linkedin.com/groups/Universidad-Laguna-2656178"><i class="fa fa-linkedin-square"></i></a></li>
 			<li><a title="Página web ULL" target="_blank" href="http://www.ull.es"><i class="fa fa-globe"></i></a></li>
 		</ul>
-	<?php
-		echo $ss_framework->close_col( 'div' );
+
+	<?php echo $ss_framework->close_col( 'div' );
         echo $ss_framework->close_row( 'div' );
-		echo $ss_framework->close_container( 'div' );
-	?>
+		echo $ss_framework->close_container( 'div' );?>
 </footer>
