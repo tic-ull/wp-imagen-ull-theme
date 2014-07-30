949ff6872ad2c347704dc8be856c308a0541ab3d
Jesús Torres <jmtorres@ull.es>
2014-07-30 16:44:24 +0100

    ya no necesitamos iconos.php

--- functions.php
+++ functions.php
@@ -3,6 +3,17 @@ require_once( dirname(__FILE__) . '/icons.php' );
 require_once( dirname(__FILE__) . '/utils.php' );
 
 /*
+ * Definiciones de las URL de los iconos de la imagen corporativa.
+ */
+
+define( 'IMAGEN_ULL_FAVICON', 		'//static.ull.es/v3/dist/img/favicon.ico');
+
+define( 'IMAGEN_ULL_APPLEICON_144', '//static.ull.es/v3/dist/img/apple-touch-icon-144-precomposed.png');
+define( 'IMAGEN_ULL_APPLEICON_114', '//static.ull.es/v3/dist/img/apple-touch-icon-114-precomposed.png');
+define( 'IMAGEN_ULL_APPLEICON_72',	'//static.ull.es/v3/dist/img/apple-touch-icon-72-precomposed.png');
+define( 'IMAGEN_ULL_APPLEICON_57',	'//static.ull.es/v3/dist/img/apple-touch-icon-57-precomposed.png');
+
+/*
  * Añadir los nuevos estilos CSS/LESS del tema.
  */
 
--- icons.php
+++ icons.php
@@ -1,14 +1,4 @@
 <?php
 
-/*
- * Definiciones de las URL de los iconos de la imagen corporativa.
- */
-
-define( 'IMAGEN_ULL_FAVICON', 		'//static.ull.es/v3/dist/img/favicon.ico');
-
-define( 'IMAGEN_ULL_APPLEICON_144', '//static.ull.es/v3/dist/img/apple-touch-icon-144-precomposed.png');
-define( 'IMAGEN_ULL_APPLEICON_114', '//static.ull.es/v3/dist/img/apple-touch-icon-114-precomposed.png');
-define( 'IMAGEN_ULL_APPLEICON_72',	'//static.ull.es/v3/dist/img/apple-touch-icon-72-precomposed.png');
-define( 'IMAGEN_ULL_APPLEICON_57',	'//static.ull.es/v3/dist/img/apple-touch-icon-57-precomposed.png');
 
 ?>
