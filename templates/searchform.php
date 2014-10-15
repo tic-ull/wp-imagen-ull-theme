176fffe5a3269252ac6b8e9019063f6b98a98dfa
Jesús Torres <jmtorres@ull.es>
2014-10-15 19:11:49 +0100

    traducir el término Buscar de la barra de busqueda

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,6 +1,6 @@
 <form role="search" class="form" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
 	<div class="input-group">
-		<input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="form-control" placeholder="<?php _e('Search', 'shoestrap'); ?> <?php bloginfo('name'); ?>">
+		<input type="search" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="form-control" placeholder="<?php _e('Buscar', 'imagen_ull'); ?> <?php bloginfo('name'); ?>">
 		<span class="input-group-btn">
 			<button type="submit" id="searchsubmit" class="btn btn-default"><i class="el-icon-search"></i></button>
 		</span>
